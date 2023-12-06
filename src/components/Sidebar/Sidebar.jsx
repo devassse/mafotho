e6d08e9f37e89672e86293737d89082ff80a0f64
João Devson Mucavel <joao.mucavel@ideiasdinamicas.com>
2023-12-06 09:30:31 +0200

    Sidebar Done

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,43 @@
-import { Box, Flex, Link } from "@chakra-ui/react";
+import { Avatar, Box, Flex, Link, Tooltip } from "@chakra-ui/react";
 import React from "react";
 import { Link as RouterLink } from "react-router-dom";
-import { InstagramLogo, InstagramMobileLogo } from "../../assets/constants";
+import {
+  CreatePostLogo,
+  InstagramLogo,
+  InstagramMobileLogo,
+  NotificationsLogo,
+  SearchLogo,
+} from "../../assets/constants";
+import { AiFillHome } from "react-icons/ai";
+import { BiLogOut } from "react-icons/bi";
 
 const Sidebar = () => {
+  const sidebarItems = [
+    {
+      icon: <AiFillHome size={25} />,
+      text: "Home",
+      link: "/",
+    },
+    {
+      icon: <SearchLogo />,
+      text: "Search",
+    },
+    {
+      icon: <NotificationsLogo />,
+      text: "Notifications",
+    },
+    {
+      icon: <CreatePostLogo />,
+      text: "Create Post",
+    },
+    {
+      icon: (
+        <Avatar size={"sm"} name="João Devson Mucavel" src="/devasse.png" />
+      ),
+      text: "Profile",
+      link: "/asaprogrammer",
+    },
+  ];
   return (
     <>
       <Box
@@ -18,7 +52,7 @@ const Sidebar = () => {
       >
         <Flex direction={"column"} gap={10} w={"full"} height={"full"}>
           <Link
-            top={"/"}
+            to={"/"}
             as={RouterLink}
             pl={4}
             display={{ base: "none", md: "block" }}
@@ -27,22 +61,73 @@ const Sidebar = () => {
             <InstagramLogo />
           </Link>
           <Link
-            top={"/"}
+            to={"/"}
             as={RouterLink}
             p={2}
             display={{ base: "block", md: "none" }}
             cursor={"pointer"}
             borderRadius={6}
-            w={10}
+            w={{ base: 10, md: "full" }}
             _hover={{
-              bg:"whiteAlpha.200"
+              bg: "whiteAlpha.200",
             }}
           >
             <InstagramMobileLogo />
           </Link>
-        </Flex>
-        <Flex direction={"column"} gap={5} cursor={"pointer"}>
-
+          <Flex direction={"column"} gap={5} cursor={"pointer"}>
+            {sidebarItems.map((item, index) => (
+              <Tooltip
+                key={index}
+                hasArrow
+                label={item.text}
+                placement="right"
+                ml={1}
+                openDelay={500}
+                display={{ base: "block", md: "none" }}
+              >
+                <Link
+                  display={"flex"}
+                  to={item.link || null}
+                  as={RouterLink}
+                  alignItems={"center"}
+                  gap={4}
+                  _hover={{ bg: "whiteAlpha.400" }}
+                  borderRadius={6}
+                  p={2}
+                  w={{ base: 10, md: "full" }}
+                  justifyContent={{ base: "center", md: "flex-start" }}
+                >
+                  {item.icon}
+                  <Box display={{ base: "none", md: "block" }}>{item.text}</Box>
+                </Link>
+              </Tooltip>
+            ))}
+          </Flex>
+          <Tooltip
+            hasArrow
+            label={"Log out"}
+            placement="right"
+            ml={1}
+            openDelay={500}
+            display={{ base: "block", md: "none" }}
+          >
+            <Link
+              display={"flex"}
+              to={"/auth"}
+              as={RouterLink}
+              alignItems={"center"}
+              gap={4}
+              _hover={{ bg: "whiteAlpha.400" }}
+              borderRadius={6}
+              p={2}
+              w={{ base: 10, md: "full" }}
+              mt={"auto"}
+              justifyContent={{ base: "center", md: "flex-start" }}
+            >
+              <BiLogOut size={25} />
+              <Box display={{ base: "none", md: "block" }}>Log out</Box>
+            </Link>
+          </Tooltip>
         </Flex>
       </Box>
     </>
