4d0512f91975c0c1549d7f9a795f268ecf463003
João Devson Mucavel <joao.mucavel@ideiasdinamicas.com>
2023-12-06 09:39:13 +0200

    Home page Building

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import { Box, Container, Flex } from "@chakra-ui/react";
+import React from "react";
+import FeedPosts from "../../components/FeedPosts/FeedPosts";
 
 const HomePage = () => {
   return (
     <>
-    <div>HomePage</div>
+      <Container mx={"container.lg"}>
+        <Flex gap={10}>
+          <Box flex={2} py={10}>
+            <FeedPosts />
+          </Box>
+          <Box
+            flex={3}
+            mr={20}
+            display={{ base: "none", lg: "block" }}
+            maxW={"300px"}
+            border={"1px solid red"}
+          >
+            Sugested
+          </Box>
+        </Flex>
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
